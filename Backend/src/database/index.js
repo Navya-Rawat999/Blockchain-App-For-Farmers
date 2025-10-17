6b89a8993c33536e0f84dca51730dc28846ce7df
Navya Rawat <navyarawat999@gmail.com>
2025-10-17 09:15:28 +0530

    Basic tools added

---
+++ Backend/src/database/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connectDB = async () => {
+  try{
+    const connectionInstance =  await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
+    console.log(`\n Mongo DB connected. DB host: ${connectionInstance.connection.host}`) // this is to check that i am connected to the right server
+  } catch (error) {
+    console.error("MONGODB connection FAILED: ", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
