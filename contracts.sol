f4cda1430dd0ebfb09bac32f4c2c54f17d5800fa
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-10-20 20:06:52 +0530

    Farmer Contract Complete

--- contracts.sol
+++ contracts.sol
@@ -9,7 +9,7 @@ contract ProduceMarket {
         address originalFarmer;
         address currentSeller;
         uint256 registrationTimeStamp;
-        uint256 currentStatus;
+        string currentStatus;
         uint256 priceinRupees;
         string originFarm;
         string QRCode;
@@ -28,6 +28,7 @@ contract ProduceMarket {
     mapping (uint256 => ProduceItem) public produceItems;
     mapping (uint256 => SaleRecord[]) public produceSaleHistory; // stores all sales for a single produce
 
+    uint256 public nextProduceId;
     // events
     event ProduceRegistered(
         uint256 indexed id,
@@ -55,4 +56,64 @@ contract ProduceMarket {
         address indexed seller,
         uint256 pricePaid
     );
+
+    constructor() {
+        nextProduceId = 1;
+    }
+
+    modifier OnlyCurrentSeller(uint256 _id){
+        require(produceItems[_id].id != 0,"Item does not exist");
+        require(msg.sender == produceItems[_id].currentSeller, "Only the current seller can call this function");
+        _;
+    }
+
+    function registerProduce(
+        string memory _name,
+        string memory _originFarm,
+        uint256 _initialPriceinINR,
+        string memory _QRCodeData
+    ) public returns(uint256){
+        require(bytes(_name).length>0,"Name Cannot be Empty");
+        require(bytes(_originFarm).length>0,"Origin Farm Cannot be Empty");
+        require(_initialPriceinINR>0,"Price must be greater than zero");
+        require(bytes(_QRCodeData).length>0,"QR Code Data Cannot be Empty");
+    
+
+    uint256 CurrentId = nextProduceId;
+    produceItems[CurrentId] = ProduceItem(
+        CurrentId,
+        _name,
+        msg.sender,   //Original Farmer is msg.sender
+        msg.sender,   //Current seller is initially the original farmer
+        block.timestamp,
+        "Harvested",
+        _initialPriceinINR,
+        _originFarm,
+        _QRCodeData
+    );
+
+    nextProduceId++;
+    emit ProduceRegistered(CurrentId, _name, msg.sender, _originFarm);
+    }
+
+    function updateProducePrice(
+        uint256 _id,
+        uint256 _newPriceInINR
+    ) public OnlyCurrentSeller(_id){
+        require(_newPriceInINR>0,"Price must be greater than zero");
+
+        produceItems[_id].priceinRupees = _newPriceInINR;
+        emit PriceUpdated(_id, _newPriceInINR, msg.sender);
+    }
+
+
+    function updateProduceStatus(
+        uint256 _id,
+        string memory _newStatus
+    ) public OnlyCurrentSeller(_id){
+        require(bytes(_newStatus).length>0,"Status Cannot be Empty");
+
+        produceItems[_id].currentStatus = _newStatus;
+        emit StatusUpdated(_id, _newStatus, msg.sender);
+    }
 }
